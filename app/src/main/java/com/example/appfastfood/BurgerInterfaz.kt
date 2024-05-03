40f12cd7e2bb474f4ef8c7f0c13ccffdfd4b81ab
Bapv28 <bapv0328@gmail.com>
2024-05-02 20:35:53 -0500

    cosas agregadas

--- app/src/main/java/com/example/appfastfood/BurgerInterfaz.kt
+++ app/src/main/java/com/example/appfastfood/BurgerInterfaz.kt
@@ -1,6 +1,12 @@
 package com.example.appfastfood
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class BurgerInterfaz : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.burger)
+    }
 }
--- app/src/main/java/com/example/appfastfood/HotdogInterfaz.kt
+++ app/src/main/java/com/example/appfastfood/HotdogInterfaz.kt
@@ -1,6 +1,12 @@
 package com.example.appfastfood
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class HotdogInterfaz : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hotdog)
+    }
 }
