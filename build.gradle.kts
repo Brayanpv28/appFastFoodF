d04d9b34791ad8bb655ffb21e26394055c7221d6
Bapv28 <bapv0328@gmail.com>
2024-05-17 17:35:00 -0500

    Comienzo base de datos

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
-import org.jetbrains.kotlin.fir.declarations.builder.buildScript
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildScript {
+/*buildScript {
     extra.apply {
         set("room_version", "2.5.2")
     }
-}
+}*/
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
