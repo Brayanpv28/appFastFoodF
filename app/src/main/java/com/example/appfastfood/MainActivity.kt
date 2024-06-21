9395fb5e719a2a683c1ebf52aea3a1f240384629
Bapv28 <bapv0328@gmail.com>
2024-06-21 04:15:57 -0500

    mejoras app

--- app/src/main/java/com/example/appfastfood/MainActivity.kt
+++ app/src/main/java/com/example/appfastfood/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.appfastfood
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registro)
+        setContentView(R.layout.registro_inicio)
+
+        val btnIS: Button = findViewById(R.id.btnInicioS)
+        btnIS.setOnClickListener {
+            val intent: Intent = Intent(this, InicioSesion::class.java)
+            startActivity(intent)
+        }
+
+        val btnReg: Button = findViewById(R.id.btnRegistro)
+        btnReg.setOnClickListener {
+            val intent: Intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
     }
 }
